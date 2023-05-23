c57fb4d7065e8a7b71b0b7b60a15b029912b6df6
abdiasiis78 <gureyxalane78@gmail.com>
2023-05-23 14:53:20 +0300

    Waxaan dhameeyey MVP ga un editka waan iskudayey lakiin waan kariwaayey

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,9 +2,39 @@ import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Notes(props) {
+  const { deleteNote, notes } = props;
+
+  
+
   return (
-    <div>
-      { /* Style your note cards with Tailwind here and get the data from app.js */ }
+    <div className="mt-7 flex flex-row gap-x-2">
+      {notes.map((note) => (
+        <div
+          key={note.id}
+          className="bg-yellow-400 p-4 rounded-md shadow-md justify-between items-center mb-4 max-w-xs"
+        >
+          <div>      
+            <div className="flex flex-col">
+              <h4 className="text-2xl text-black font-bold">{note.title}</h4>
+              <p className="text-black  text-justify p-1/2 text-1xl font-normal">{note.content} </p>
+            </div>
+            <div className="text-center">
+              <button
+                onClick={() => deleteNote(note.id)}
+                className="text-black hover:text-red-700 mr-2 "
+              >
+                <FaTrash />
+              </button>
+              <button
+              
+                className="text-black hover:text-blue-700"
+              >
+                <FaEdit />
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
